d895622f3ec5dd3e55246a22d7aacc702bac1d6a
Riku <101910815+RikuDelicious@users.noreply.github.com>
2023-02-03 10:38:51 +0900

    テスト追加

--- test_overriding.py
+++ test_overriding.py
@@ -31,18 +31,55 @@ def test_override_context_manager(container: Container):
     API Clientをモックオブジェクトと差し替えるためにProvider.override()メソッドをコンテキストマネージャーとして使う
     """
     with container.api_client_factory.override(unittest.mock.Mock(ApiClient)):
-        service = container.service_factory()
-        assert isinstance(service.api_client, unittest.mock.Mock)
-
-
-def test_reset_override(container: Container):
-    """
-    上書きした状態から元に戻すためにProvider.reset_override()メソッドを使う
-    """
-    container.api_client_factory.override(providers.Factory(ApiClientStub))
-    service1 = container.service_factory()
-    container.api_client_factory.reset_override()
+        service1 = container.service_factory()
     service2 = container.service_factory()
 
-    assert isinstance(service1.api_client, ApiClientStub)
+    assert isinstance(service1.api_client, unittest.mock.Mock)
     assert isinstance(service2.api_client, ApiClient)
+
+
+def test_reset_override_stack(container: Container):
+    """
+    Providerの上書きは複数回行うことができ、スタック構造のように積み重なる
+    上書きした状態から元に戻すためには、Provider.reset_override()メソッドを使う
+    """
+    service1 = container.service_factory()
+
+    container.api_client_factory.override(providers.Factory(ApiClientStub))
+    service2 = container.service_factory()
+
+    container.api_client_factory.override(unittest.mock.Mock(ApiClient))
+    service3 = container.service_factory()
+
+    container.api_client_factory.reset_override()
+    service4 = container.service_factory()
+
+    assert isinstance(service1.api_client, ApiClient)
+    assert isinstance(service2.api_client, ApiClientStub)
+    assert isinstance(service3.api_client, unittest.mock.Mock)
+    assert isinstance(service4.api_client, ApiClient)
+
+
+def test_rest_last_overriding(container: Container):
+    """
+    Provider.reset_last_overriding()メソッドで上書きする一つ前の状態へ戻る
+    """
+    service1 = container.service_factory()
+
+    container.api_client_factory.override(providers.Factory(ApiClientStub))
+    service2 = container.service_factory()
+
+    container.api_client_factory.override(unittest.mock.Mock(ApiClient))
+    service3 = container.service_factory()
+
+    container.api_client_factory.reset_last_overriding()
+    service4 = container.service_factory()
+
+    container.api_client_factory.reset_last_overriding()
+    service5 = container.service_factory()
+
+    assert isinstance(service1.api_client, ApiClient)
+    assert isinstance(service2.api_client, ApiClientStub)
+    assert isinstance(service3.api_client, unittest.mock.Mock)
+    assert isinstance(service4.api_client, ApiClientStub)
+    assert isinstance(service5.api_client, ApiClient)
